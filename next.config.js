d905aaea2fe9cdadfca9e0604f444612f26dadad
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2022-05-25 19:09:13 +0200

    basePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   assetPrefix: "/online-portfolio-v2/",
+  basePath: "/online-portfolio-v2",
   reactStrictMode: false,
   images: {
     domains: ["avatars.githubusercontent.com"],
