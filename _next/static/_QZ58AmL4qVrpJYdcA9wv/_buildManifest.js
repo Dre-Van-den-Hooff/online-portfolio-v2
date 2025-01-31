b2d77ba01f2247c487bcd831c33178330d2edb78
Dre-Van-den-Hooff <79202150+Dre-Van-den-Hooff@users.noreply.github.com>
2025-01-31 21:22:35 +0000

    Deploying to gh-pages from @ Dre-Van-den-Hooff/online-portfolio-v2@3aeb32e0a696edc1dbf8f17600b73f37d4c0ace4 🚀

---
+++ _next/static/_QZ58AmL4qVrpJYdcA9wv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8768c3735de4a87d3c4a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
